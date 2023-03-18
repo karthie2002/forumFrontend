c230bf7c07d3740c421f4d71801954e5a35ee303
KE2002 <44949462+KE2002@users.noreply.github.com>
2023-03-18 16:05:45 +0530

    l

--- src/app/auth-layout/login-layout/login-layout.component.ts
+++ src/app/auth-layout/login-layout/login-layout.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-login-layout',
   templateUrl: './login-layout.component.html',
-  styleUrls: ['./login-layout.component.scss']
+  styleUrls: ['./login-layout.component.scss'],
 })
 export class LoginLayoutComponent {
-
+  @Input() isLogin: boolean = true;
 }
