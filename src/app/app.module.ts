ecb7f65808c17c3dcee1e9ba2bfd7cebd6d61d35
KE2002 <44949462+KE2002@users.noreply.github.com>
2023-03-18 15:37:30 +0530

    new login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './auth-layout/login/login.component';
+import { LoginLayoutComponent } from './auth-layout/login-layout/login-layout.component';
+import { SignupComponent } from './auth-layout/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    LoginLayoutComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
