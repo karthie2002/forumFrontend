c78dc8daa40ba1d89d16fcff682e21890403a729
Aishwarya <riya129ash@gmail.com>
2023-03-18 16:46:56 +0530

    Question

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MainpageComponent } from './mainpage/mainpage.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './auth-layout/login/login.component';
+import { QuestionComponent } from './components/question/question.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { LoginComponent } from './auth-layout/login/login.component';
     MainpageComponent,
     SidebarComponent,
     NavbarComponent,
+    QuestionComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
