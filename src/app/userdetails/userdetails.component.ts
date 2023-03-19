ad86f25f3ef4301df7d110c361ee558c7374cd25
KE2002 <44949462+KE2002@users.noreply.github.com>
2023-03-19 18:53:32 +0530

    x

--- src/app/userdetails/userdetails.component.ts
+++ src/app/userdetails/userdetails.component.ts
@@ -6,43 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./userdetails.component.scss'],
 })
 export class UserdetailsComponent {
-  pattern: string = '../../assets/images/pattern.png';
-  url: string = '../../assets/images/user.png';
-  isClick: boolean = false;
-  techCount: number = 0;
-  checkboxestech = [
-    {
-      id: '8',
-      label: 'Asia',
-      isChecked: true,
-    },
-    {
-      id: '9',
-      label: 'Europe',
-      isChecked: false,
-    },
-    {
-      id: '10',
-      label: 'North America',
-      isChecked: false,
-    },
-    {
-      id: '11',
-      label: 'South America',
-      isChecked: false,
-    },
-    {
-      id: '12',
-      label: 'Australia',
-      isChecked: false,
-    },
-    {
-      id: '13',
-      label: 'Africa',
-      isChecked: true,
-    },
-  ];
-
   uploadFile(event: any) {
     if (event.target.files) {
       var reader = new FileReader();
@@ -53,4 +16,7 @@ export class UserdetailsComponent {
       };
     }
   }
+
+  pattern: string = '../../assets/images/pattern.png';
+  url: string = '../../assets/images/user.png';
 }
