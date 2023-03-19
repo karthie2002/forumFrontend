7ccc9b1b1d2556d9d3e8f8228faa2c2e5bacc1cd
Akash <67492990+Akash-Manikandan@users.noreply.github.com>
2023-03-19 19:32:16 +0530

    lusu

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
+  @Input() userProfile: string =
+    'https://api.dicebear.com/5.x/lorelei/svg?seed=Snowball';
 }
