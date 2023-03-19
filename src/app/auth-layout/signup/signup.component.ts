d68e89e34101349dcdc2207ae2335b37bc99e1e1
KE2002 <44949462+KE2002@users.noreply.github.com>
2023-03-19 18:45:44 +0530

    d

--- src/app/auth-layout/signup/signup.component.ts
+++ src/app/auth-layout/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,8 +7,12 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent {
+  constructor(private router: Router) {}
   @Output() authData = new EventEmitter<boolean>();
   public btnClick(): void {
     this.authData.emit(true);
+  }
+  onButtonClick(): void {
+    this.router.navigate(['/user-details']);
   }
 }
