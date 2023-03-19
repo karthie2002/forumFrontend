81fa5913d47e3018215bcfe6f8289a9373d04a69
Aishwarya <riya129ash@gmail.com>
2023-03-19 19:38:49 +0530

    Question page kinda ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginLayoutComponent } from './auth-layout/login-layout/login-layout.component';
-import { MainpageComponent } from './mainpage/mainpage.component';
+import { HomepageComponent } from './homepage/homepage.component';
 import { ProfileComponent } from './profile/profile.component';
 import { UserdetailsComponent } from './userdetails/userdetails.component';
 
 const routes: Routes = [
   { path: 'login-layout', component: LoginLayoutComponent },
-  { path: 'main-page', component: MainpageComponent },
+  { path: 'home-page', component: HomepageComponent },
   { path: 'user-details', component: UserdetailsComponent },
   { path: 'profile', component: ProfileComponent },
   { path: '', redirectTo: '/login-layout', pathMatch: 'full' },
