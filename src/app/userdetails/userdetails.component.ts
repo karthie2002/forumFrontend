86a830329ef720d148945e2e6512ec0d6919e209
KE2002 <44949462+KE2002@users.noreply.github.com>
2023-03-19 22:15:50 +0530

    n

--- src/app/userdetails/userdetails.component.ts
+++ src/app/userdetails/userdetails.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-userdetails',
@@ -6,6 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./userdetails.component.scss'],
 })
 export class UserdetailsComponent {
+  pattern: string = '../../assets/images/pattern.png';
+  url: string = '../../assets/images/user.png';
+  technology: string[] = ['C', 'C++'];
+  recommendationControl = new FormControl();
+  clearRecommendation(index: number) {
+    if (index > -1) {
+      this.technology.splice(index, 1);
+    }
+  }
+  addRecommendation(event: Event) {
+    event.preventDefault();
+    if (event != null) {
+      const value: string = this.recommendationControl.value;
+      if (value.length > 0) {
+        this.technology = [...this.technology, value];
+        this.recommendationControl.reset();
+      }
+    }
+  }
   uploadFile(event: any) {
     if (event.target.files) {
       var reader = new FileReader();
@@ -16,7 +36,4 @@ export class UserdetailsComponent {
       };
     }
   }
-
-  pattern: string = '../../assets/images/pattern.png';
-  url: string = '../../assets/images/user.png';
 }
