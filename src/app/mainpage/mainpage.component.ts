b2e4b8306215c42964a7cc7920fe04a157994c6e
Aishwarya <riya129ash@gmail.com>
2023-03-25 22:35:15 +0530

    Aishwarya

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import {
   GetAllProblems,
   MainPageService,
 } from '../service/main-page/main-page.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { NotifierService } from '../service/notifier.service';
+
+export interface Question {
+  question: string;
+  description: string;
+  quesImg: string;
+}
+
 @Component({
   selector: 'app-mainpage',
   templateUrl: './mainpage.component.html',
@@ -13,7 +22,15 @@ export class MainpageComponent implements OnInit {
   constructor(
     private router: Router,
     private mainPageService: MainPageService,
+    private notifierService: NotifierService,
+    private route: ActivatedRoute
   ) {}
+
+  questionForm = new FormGroup({
+    question: new FormControl(),
+    description: new FormControl(),
+    quesImg: new FormControl(),
+  });
 
   questionList: GetAllProblems[] = [];
   isShown: boolean = false;
@@ -39,6 +56,24 @@ export class MainpageComponent implements OnInit {
     this.isShown = true;
   }
   submit(event: any) {
+    event.preventDefault();
+    const values: Question = JSON.parse(
+      JSON.stringify(this.questionForm.value)
+    );
+    console.log(values);
+    if (values.question == null || values.question.trim() == '') {
+      this.notifierService.showNotification('Question field cannot be empty');
+    }
+    if (values.description == null || values.question.trim() == '') {
+      this.notifierService.showNotification(
+        'Question Description field cannot be empty'
+      );
+    }
+    if (values.quesImg == null || values.question.trim() == '') {
+      this.notifierService.showNotification(
+        'Question Image field cannot be empty'
+      );
+    }
     this.isShown = false;
   }
 }
