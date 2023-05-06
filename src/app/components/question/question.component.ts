4d637bb745017298b9e24f5486e4311aec069444
Akash <67492990+Akash-Manikandan@users.noreply.github.com>
2023-05-06 14:46:33 +0530

    okk

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import * as Prism from 'prismjs';
 
 @Component({
   selector: 'app-question',
@@ -13,6 +14,8 @@ export class QuestionComponent {
   @Input() date: number = 0;
   @Input() userProfile: string = '';
   @Input() problemImg: string = '';
-  constructor() {}
+  constructor() {
+    
+  }
 
 }
