c6cdd2128c32e7515257c59c0ade458361c0c6bb
Aishwarya <riya129ash@gmail.com>
2023-05-13 17:52:33 +0530

    Popup declared

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,9 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import {
-  AddCategory,
-  QuestionService,
-} from 'src/app/service/main-page/question.service';
 import { NotifierService } from 'src/app/service/notifier.service';
 
 @Component({
@@ -19,21 +14,33 @@ export class QuestionComponent {
   @Input() date: number = 0;
   @Input() userProfile: string = '';
   @Input() problemImg: string = '';
+
   isAddCateg: boolean = false;
-  constructor(
-    private questionService: QuestionService,
-    private notifierService: NotifierService
-  ) {}
+  constructor(private notifierService: NotifierService) {}
 
   addCateg(event: Event) {
     event.stopPropagation();
-    // console.log('addcateg', this.category, this.question);
-    if (this.question != null || this.category != null) {
-      this.isAddCateg = !this.isAddCateg;
+    const storage = localStorage.getItem('userData');
+    const name =
+      storage == null
+        ? ''
+        : JSON.parse(localStorage.getItem('userData')!).username;
+
+    if (this.username == name) {
+      if (this.question != null || this.category != null) {
+        this.isAddCateg = !this.isAddCateg;
+      } else {
+        this.notifierService.showNotification(
+          'Question Description field cannot be empty'
+        );
+      }
     } else {
       this.notifierService.showNotification(
-        'Question Description field cannot be empty'
+        'You cannot add category to a question that is not posted by you'
       );
     }
+  }
+  closeCateg() {
+    this.isAddCateg = false;
   }
 }
